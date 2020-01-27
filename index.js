46bac146f4db1ca100ffd4f894fcd06c2160a461
Alexandra Pestruyeva <apestruy@gmail.com>
2020-01-27 18:28:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,12 @@ function ternaryCheckCity(city){
 }
 
 function switchOnCharmFromTip(){
-  
+  switch (generosity) {
+    case "generous": return "Thank you so much.";
+    break;
+    case "not as generous": return "Thank you.";
+    break;
+    default: return "Bye.";
+    break;
+  }
 }
